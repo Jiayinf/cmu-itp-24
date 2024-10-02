f16f9477080b7426e6a2ccc25c0b624195956cb3
Jeremy Avigad <avigad@cmu.edu>
2024-10-02 16:15:20 -0400

    add lecture1002

--- CmuItp24/lectures/lecture1002.lean
+++ CmuItp24/lectures/lecture1002.lean
@@ -23,7 +23,7 @@ namespace set_sequences
 
 variable {α : Type*}
 variable (A C : ℕ → Set α)
-variable (C_def : ∀ n, C n = A n \ (⋃ i < n, A i))
+variable (C_def : ∀ n, C n = A n \ ⋃ i < n, A i)
 
 section
 open Classical
@@ -42,7 +42,12 @@ theorem Union_C_eq_Union_A : (⋃ i, C i) = (⋃ i, A i) := by
     rw [C_def n] at xmem
     simp at xmem
     use n, xmem.1
-  sorry
+  intro h
+  -- have := aux h
+  -- rcases this with ⟨i, hi, hi'⟩
+  rcases aux h with ⟨i, hi, hi'⟩
+  simp [C_def]  -- can use simp? to clean up
+  use i
 
 end
 
@@ -62,16 +67,40 @@ variable (s t u : Finset ℕ)
 open Finset
 
 example : s ∩ (t ∪ u) ⊆ (s ∩ t) ∪ (s ∩ u) := by
-  sorry
+  intro x
+  rw [mem_inter, mem_union, mem_union, mem_inter, mem_inter]
+  tauto
+
+example : s ∩ (t ∪ u) ⊆ (s ∩ t) ∪ (s ∩ u) := by
+  intro x
+  -- simp?
+  simp only [mem_inter, mem_union, and_imp]
+  tauto
 
 example : s ∩ (t ∪ u) = (s ∩ t) ∪ (s ∩ u) := by
-  sorry
+  ext x
+  simp; tauto
+
+#eval range 10
+#eval Finset.card (range 10)
+#eval (range 10).card
+#eval (range 10).card.gcd 5
+
+#eval (range 20).filter (fun x => x^2 < 18)
+#eval (range 20).filter Even
+#eval (range 20).filter Odd
+#eval (range 200).filter Nat.Prime
+
+#eval ((range 20).filter Odd).card
+#eval (range 20).filter Odd |>.card
 
 end
 
 /-
 More induction on the natural numbers.
 -/
+
+#check Nat.factorial
 
 def fac : ℕ → ℕ
   | 0     => 1
@@ -80,18 +109,18 @@ def fac : ℕ → ℕ
 section
 
 variable {α : Type*} (s : Finset ℕ) (f : ℕ → ℕ) (n : ℕ)
+open Finset
 
 #check Finset.sum s f
 #check Finset.prod s f
 
 #eval Finset.sum (Finset.range 10) (fun n => n^2)
+#eval range 10 |>.sum fun n => n^2
 
 open BigOperators
-open Finset
-
-#eval Finset.sum (Finset.range 10) (fun n => n^2)
 
 #eval ∑ n ∈ range 10, n^2
+#eval ∑ n in range 10, n^2   -- old notation
 
 example : s.sum f = ∑ x ∈ s, f x := rfl
 example : s.prod f = ∏ x ∈ s, f x := rfl
@@ -100,93 +129,44 @@ example : (range n).sum f = ∑ x ∈ range n, f x := rfl
 example : (range n).prod f = ∏ x ∈ range n, f x := rfl
 
 example (f : ℕ → ℕ) : ∑ x ∈ range 0, f x = 0 := by
-  sorry
+  -- rw [sum_range_zero]
+  rfl
 
 example (f : ℕ → ℕ) (n : ℕ): ∑ x ∈ range n.succ, f x = (∑ x ∈ range n, f x) + f n := by
-  sorry
+  -- rw?
+  rw [sum_range_succ]
+  -- rfl doesn't work
 
 example (f : ℕ → ℕ) : ∏ x ∈ range 0, f x = 1 :=
-  sorry
+  rfl
 
 example (f : ℕ → ℕ) (n : ℕ): ∏ x ∈ range n.succ, f x = (∏ x ∈ range n, f x) * f n := by
-  sorry
+  rw [prod_range_succ]
 
 example (n : ℕ) : fac n = ∏ i ∈ range n, (i + 1) := by
-  sorry
+  induction' n with n ih
+  . rfl
+    -- simp [fac]
+  rw [fac, prod_range_succ, ih, mul_comm]
+  --simp [fac, ih, mul_comm]
 
 theorem sum_id (n : ℕ) : ∑ i ∈ range (n + 1), i = n * (n + 1) / 2 := by
-  sorry
+  -- apply?
+  apply Nat.eq_div_of_mul_eq_right
+  . simp -- norm_num
+  induction' n with n ih
+  . rfl
+  rw [sum_range_succ, mul_add, ih]
+  ring
+
+-- same proof!
 
 theorem sum_sqr (n : ℕ) : ∑ i ∈ range (n + 1), i^2 = n * (n + 1) * (2 *n + 1) / 6 := by
-  sorry
-
-end
-
-/-
-Another style of writing proofs by structural induction.
--/
-
-section
-
-variable {α β γ : Type*}
-variable (as bs cs : List α)
-variable (a b c : α)
-
-open List
-
-theorem append_nil' : as ++ [] = as := by
-  induction' as with a as ih
+  apply Nat.eq_div_of_mul_eq_right
+  . simp
+  induction' n with n ih
   . rfl
-  . rw [cons_append, ih]
-
--- do the lightbulb trick!
-
-/-
-Another example:
--/
-
-#eval map (fun n => n^2) [1, 2, 3, 4, 5]
-
--- We write the argument that doesn't vary in recursive calls on the left-hand side of `:` (the colon).
-def map' (f : α → β) : List α → List β
-  | []      => []
-  | a :: as => f a :: map' f as
-
-#eval map' (fun n => n^2) [1, 2, 3, 4, 5]
-
-theorem map'_map' (f : α → β) (g : β → γ) (as : List α) :
-    map' g (map' f as) = map' (g ∘ f) as := by
-  induction' as with a as ih
-  . rfl
-  .  simp [map', ih]
-
--- do it again!
-
-/-
-Reversing a list.
--/
-
-def bad_reverse : List α → List α
-  | []      => []
-  | a :: as => concat (bad_reverse as) a
-
-/-- `reverse_aux as acc` reverses `as` and appends `acc` -/
-def reverse_aux : List α → List α → List α
-  | [],      acc => acc
-  | a :: as, acc => reverse_aux as (a :: acc)
-
-def reverse' (as : List α) := reverse_aux as []
-
-theorem reverse_aux_append (as bs cs : List α) :
-    reverse_aux (as ++ bs) cs = reverse_aux bs (reverse_aux as cs) := by
-  sorry
-
-theorem reverse_aux_append' (as bs cs : List α) :
-    reverse_aux as (bs ++ cs) = (reverse_aux as bs) ++ cs := by
-  sorry
-
-theorem reverse'_append (as bs : List α) :
-    reverse' (as ++ bs) = reverse' bs ++ reverse' as := by
-  sorry
+  rw [sum_range_succ, mul_add, ih]
+  ring
 
 end
