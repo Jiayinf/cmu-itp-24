c7521f5dab85cfca510cb2829f2ec9ea13302dab
Jeremy Avigad <avigad@cmu.edu>
2024-11-11 11:37:31 -0500

    update lecture1111

--- CmuItp24/lectures/lecture1111.lean
+++ CmuItp24/lectures/lecture1111.lean
@@ -1,15 +1,12 @@
 import Mathlib.Data.Real.Basic
-import Mathlib.Algebra.BigOperators.Group.Finset
-import Mathlib.Order.Monotone.Basic
-import Mathlib.Algebra.Order.Ring.Basic
-import Mathlib.Tactic
-import Mathlib.Probability.Integration
-import Mathlib.Probability.ConditionalProbability
-import Mathlib.Data.Finset.Basic
-import Mathlib.Probability.ProbabilityMassFunction.Basic
 
 /-
 # Announcements
+
+Today's lecture: on the blacboard!
+Notes in lecture1111.lean, Foundations.lean.
+
+We are aiming to grade the first project by next Monday.
 
 The final project is due on Friday, December 6, the last day of class.
 
@@ -30,8 +27,20 @@ Office hours for the rest of the semester:
   Thursday, 4-5 (Eric)
   Friday, 12-1 (Jeremy)
 
+Wednesday: Lean as a programming language
+Next Monday (Wojciech): Lean as a metaprogramming language (writing tactics)
+
+Beyond that:
+- automation (in Lean and beyond)
+- more tips and tricks
+
+Kevin Buzzard will give a math colloquium talk on December 6.
+
+Courses of interest next semester:
+- 15-311: Logic and Mechanized Reasoning
+- 15-414: Bug Catching
+
 # Agenda
 
 Continue with Lean's axiomatic foundation, in Foundations.Lean.
-
 -/
