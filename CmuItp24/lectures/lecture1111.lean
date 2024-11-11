dc6957e0c2212f78a636c909ccd62f46390c6153
Jeremy Avigad <avigad@cmu.edu>
2024-11-10 19:14:13 -0500

    add lecture1111 and Foundations

---
+++ CmuItp24/lectures/lecture1111.lean
@@ -0,0 +1,37 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Algebra.BigOperators.Group.Finset
+import Mathlib.Order.Monotone.Basic
+import Mathlib.Algebra.Order.Ring.Basic
+import Mathlib.Tactic
+import Mathlib.Probability.Integration
+import Mathlib.Probability.ConditionalProbability
+import Mathlib.Data.Finset.Basic
+import Mathlib.Probability.ProbabilityMassFunction.Basic
+
+/-
+# Announcements
+
+The final project is due on Friday, December 6, the last day of class.
+
+Don't put work off until the last minute!
+
+Tell us by Monday, November 11 what you plan to do.
+
+For the rest of the semester:
+  lectures on Monday and Wednesday
+  extra office hour on Friday
+
+Office hours for the rest of the semester:
+
+  Monday, 2-3 (Jeremy)
+  Tuesday, 3-4 (Wojciech)
+  Wednesday, 11-12 (Wojciech)
+  Wednesday, 3:30-4:30 (Eric)
+  Thursday, 4-5 (Eric)
+  Friday, 12-1 (Jeremy)
+
+# Agenda
+
+Continue with Lean's axiomatic foundation, in Foundations.Lean.
+
+-/
